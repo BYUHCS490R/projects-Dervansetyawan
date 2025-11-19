a5b6797083cc8590925758224ff38f25c4377f18
Dervansetyawan <dervan30ardana.gmail.com>
2025-11-19 10:58:20 -1000

    j

--- activities/formprocessing/formprocessing.js
+++ activities/formprocessing/formprocessing.js
@@ -1,6 +1,6 @@
 document.getElementById('myForm').addEventListener('submit',function(event) {
     event.preventDefault();
-    alert("Form Submitted");
+    // alert("Form Submitted");
 
     const fullname = document.getElementById('fname').value;
     const email = document.getElementById('email').value;
